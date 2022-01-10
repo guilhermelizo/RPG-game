816ef1703aad36e00bd55c1b4bc10345ae8cdb57
Guilherme Lizo <81538444+guilhermelizo@users.noreply.github.com>
2022-01-10 18:25:49 -0300

    Add files via upload

---
+++ src/hooks/useCharacter.ts
@@ -0,0 +1,61 @@
+import { useState } from "react";
+import { CharacterSides } from "../types/CharacterSides";
+import { mapSpots } from "../data/mapSpots";
+
+// Hook personalizado, não entrega nada gráfico
+// Apenas entrega propriedades
+// (Posição do usuário)
+export const useCharacter = () => {
+  const [pos, setPos] = useState({ x: 3, y: 5 });
+  const [side, setSide] = useState<CharacterSides>("down");
+
+  const moveLeft = () => {
+    setPos(pos => ({ 
+        x: canMove(pos.x - 1, pos.y) ? pos.x - 1 : pos.x,
+        y: pos.y
+      }));
+    setSide("left");
+  }
+  const moveRight = () => {
+    setPos(pos => ({ 
+        x: canMove(pos.x + 1, pos.y) ? pos.x + 1 : pos.x,
+        y: pos.y,
+      }));
+    setSide("right");
+  };
+  const moveDown = () => {
+    setPos(pos => ({ 
+        x: pos.x,
+        y: canMove(pos.x, pos.y + 1) ? pos.y + 1 : pos.y,
+      }));
+    setSide("down");
+  };
+  const moveUp = () => {
+    setPos(pos => ({ 
+        x: pos.x,
+        y: canMove(pos.x, pos.y - 1) ? pos.y - 1 : pos.y,
+      }));
+    setSide("up");
+  };
+
+  //Função que vai verificar se o personagem pode ou não se mover de acordo com o mapa
+  const canMove = (x: number, y: number) => {
+    if (x < 0 || y < 0) return false;
+
+    if(mapSpots[y][x] === 1){
+        return true;
+    }
+
+    return false;
+  };
+
+  return {
+    x: pos.x,
+    y: pos.y,
+    side,
+    moveLeft,
+    moveRight,
+    moveDown,
+    moveUp,
+  };
+};
